e0e464a7f0973490df1da43a19ac938e527264b9
giftformekft-tech <giftformekft@gmail.com>
2025-10-16 08:26:56 +0200

    Delete inc directory

--- inc/shortcode.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined('ABSPATH') ) exit;
-
-add_shortcode('nb_designer', function(){
-  ob_start();
-  include NB_DESIGNER_PATH.'templates/designer-page.php';
-  return ob_get_clean();
-});
